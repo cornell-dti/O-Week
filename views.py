a9a23df1a9cd70cf322707da39de888e64cd4fa7
Arnav Ghosh <garnav@gmail.com>
2017-05-04 11:21:46 -0400

    Json Resonse converted to API Json Responses

--- views.py
+++ views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.core.serializers.json import DjangoJSONEncoder
 from django.core import serializers
-import json
+import json, os
 
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -10,8 +10,10 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import EventSerializer, CategorySerializer
-
 from .models import EventDetail, Category, EventDetailForm, EventCategories
+from wsgiref.util import FileWrapper
+from django.conf import settings
+
 
 def index(request):
     return JsonResponse("Hello, world. You're at the flow index.", safe = False)
@@ -19,19 +21,19 @@ def index(request):
 @api_view(['GET'])
 def feed(request, day, req_category = "NONE"):
     if request.method == 'GET':
-		if req_category.lower() == "none":
-			#returns eveything for storage on the app -> do we want to change this to a refreshing one
-			event_set = EventDetail.objects.filter(start_date__day = str(day)) #because its a five day event
-		else:
-			cat_object = Category.objects.filter(category = req_category)
-			corr_events = EventCategories.filter(category = cat_object).values_list()
-			event_set = EventDetail.objects.none()
-			for event in corr_events:
-				spec_event = EventDetail.objects.filter(start_date__day = str(day)).filter(id = event)
-				event_set.union(spec_event)
-		
-		serializer = EventSerializer(event_set, many = True)
-		return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe = False)
+        if req_category.lower() == "none":
+            #returns eveything for storage on the app -> do we want to change this to a refreshing one
+            event_set = EventDetail.objects.filter(start_date__day = str(day)) #because its a five day event
+        else:
+            cat_object = Category.objects.filter(category = req_category)
+            corr_events = EventCategories.filter(category = cat_object).values_list()
+            event_set = EventDetail.objects.none()
+            for event in corr_events:
+                spec_event = EventDetail.objects.filter(start_date__day = str(day)).filter(id = event)
+                event_set.union(spec_event)
+            
+        serializer = EventSerializer(event_set, many = True)
+        return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe = False)
 
 @api_view(['GET'])
 def event_details(request, event_id):
@@ -42,9 +44,20 @@ def event_details(request, event_id):
 @api_view(['GET'])
 def categories(request):
 	category_set = Category.objects.all().order_by('category')
-	print type(category_set)
 	serializer = CategorySerializer(category_set, many = True)
 	return JsonResponse(serializer.data, status=status.HTTP_200_OK, safe = False)
+
+@api_view(['GET'])
+def eventImage(request, event_id):
+	event = EventDetail.objects.filter(pk = event_id)[0].images.name #assumes that it returns a correct one for now
+	file_path = os.path.join(settings.MEDIA_ROOT, event)
+	if os.path.exists(file_path):
+		with open(file_path, 'rb') as fh:
+			response = HttpResponse(fh.read(), content_type="image/jpg")
+			response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+			return response
+	else:
+		raise HttpResponse("")
 	
 def add_event(request):
 	if request.method == 'POST':
