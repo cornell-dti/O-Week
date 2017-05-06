53d81b981ea9751a1141f0f708f593a990d935ca
Arnav Ghosh <garnav@gmail.com>
2017-05-06 19:55:59 -0400

    removed cast from config

--- flow/settings.py
+++ flow/settings.py
@@ -20,7 +20,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 #SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False)
 DATABASES = {
     'default': dj_database_url.config(
         default=config('DATABASE_URL')
