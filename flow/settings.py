7e255c9ea4dcbc715d37e3ce30fa5ac73ba448b1
Arnav Ghosh <garnav@gmail.com>
2017-05-12 01:41:53 -0400

    a

--- flow/settings.py
+++ flow/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
+
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False)
 DATABASES = {
