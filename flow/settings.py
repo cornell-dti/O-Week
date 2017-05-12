885752ab96b450fed1a5929a17d2f57f225efbd7
Arnav Ghosh <garnav@gmail.com>
2017-05-12 02:08:21 -0400

    p

--- flow/settings.py
+++ flow/settings.py
@@ -38,7 +38,7 @@ DATABASES = {
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
