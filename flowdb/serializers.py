aefe1c908fd165acdb279937990d5c7ddc12ee5a
Arnav Ghosh <garnav@gmail.com>
2017-05-26 02:00:19 -0400

    serializer changes

--- flowdb/serializers.py
+++ flowdb/serializers.py
@@ -6,9 +6,9 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = EventDetail
         fields = ('pk', 'name', 'description', 'location', 'category', 'start_date', 'end_date', 'start_time', 'end_time')
-		
+        
 class CategorySerializer(serializers.ModelSerializer):
 	
 	class Meta:
 		model = Category
-        fields = ('category', 'description')
+		fields = ('category', 'description')
