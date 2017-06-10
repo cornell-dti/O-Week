af28f2cd19be8a6516d8a77190147f734d22f250
Arnav Ghosh <garnav@gmail.com>
2017-06-10 21:20:04 +0530

    admin

--- flowdb/admin.py
+++ flowdb/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
-from .models import Category, EventDetail, Tag, EventTags, EventCategories
+from .models import Category, EventDetail, Tag, EventTags
 # Register your models here.
 
 admin.site.register(Category)
 admin.site.register(EventDetail)
 admin.site.register(Tag)
 admin.site.register(EventTags)
-admin.site.register(EventCategories)
