8f862106f023d46c7326bae7dd7a7fe5bcfacda5
Arnav Ghosh <garnav@gmail.com>
2017-06-11 13:08:36 +0530

    removed try except for try purposes

--- flowdb/views.py
+++ flowdb/views.py
@@ -60,24 +60,24 @@ def categories(request):
 
 @api_view(['GET'])
 def eventImage(request, event_id):
-	try:
-		event = EventDetail.objects.filter(pk = event_id)[0].images.name #assumes that it returns a correct one for now
-		s3 = S3Connection(settings.AWS_ACCESS_KEY_ID, settings, AWS_SECRET_ACCESS_KEY)
-		s3bucket = s3.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
-		s3key = s3bucket.get_key(event)
-		fp = tempfile.TemporaryFile()
-		s3file = s3key.get_file(fp)
-		with open(s3file, 'rb') as fh:
-			response = HttpResponse(fh.read(), status=status.HTTP_200_OK, content_type="image/jpg") #what if its not jpg
-			response['Content-Disposition'] = 'inline; filename=' + event
-			return response
+	#try:
+	event = EventDetail.objects.filter(pk = event_id)[0].images.name #assumes that it returns a correct one for now
+	s3 = S3Connection(settings.AWS_ACCESS_KEY_ID, settings, AWS_SECRET_ACCESS_KEY)
+	s3bucket = s3.get_bucket(settings.AWS_STORAGE_BUCKET_NAME)
+	s3key = s3bucket.get_key(event)
+	fp = tempfile.TemporaryFile()
+	s3file = s3key.get_file(fp)
+	with open(s3file, 'rb') as fh:
+		response = HttpResponse(fh.read(), status=status.HTTP_200_OK, content_type="image/jpg") #what if its not jpg
+		response['Content-Disposition'] = 'inline; filename=' + event
+		return response
 #		file_path = os.path.join(settings.MEDIA_ROOT, event)
 #		with open(file_path, 'rb') as fh:
 #			response = HttpResponse(fh.read(), status=status.HTTP_200_OK, content_type="image/jpg") #what if its not jpg
 #			response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
 #			return response
-	except:
-		return HttpResponse("",status = status.HTTP_400_BAD_REQUEST)
+	#except:
+	#	return HttpResponse("",status = status.HTTP_400_BAD_REQUEST)
 	
 def bulk_add(request):
 	if request.method == 'POST':
